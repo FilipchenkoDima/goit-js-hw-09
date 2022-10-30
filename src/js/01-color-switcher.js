2e12bf8fcad85e9c08bc1cb560a8c5edcae30a1e
FilipchenkoDima <blewdinor@gmail.com>
2022-10-30 23:04:43 +0200

    Done 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+startBtn = document.querySelector('[data-start]');
+stopBtn = document.querySelector('[data-stop]');
+bodyEl = document.querySelector('body');
+let bodyColor = null;
+
+startBtn.addEventListener('click', onChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
+
+function onChangeColor() {
+    bodyColor = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute('disabled', 'true');
+    stopBtn.removeAttribute('disabled');
+};
+
+function onStopChangeColor() {
+    clearInterval(bodyColor);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'true');
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmitInputs);
+
+function onSubmitInputs(evt) {
+  evt.preventDefault();
+
+  const { delay, step, amount } = evt.target;
+  const firstDelay = Number(delay.value);
+  const delayStep = Number(step.value);
+  const amountEl = Number(amount.value);
+
+  for (let i = 1; i <= amountEl; i++) {
+    
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  };
+};
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+

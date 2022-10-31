b96ef5a3079c1edf66d377fdbf7c59be470bd741
FilipchenkoDima <105314903+FilipchenkoDima@users.noreply.github.com>
2022-10-31 18:18:47 +0000

    Deploying to gh-pages from @ FilipchenkoDima/goit-js-hw-09@11dfc5de5b8f259fefee641c80a5cd6e711f383e 🚀

---
+++ 02-timer.5c536d63.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("[data-start]");console.log(o);
+//# sourceMappingURL=02-timer.5c536d63.js.map
---
+++ 02-timer.8f0c8199.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("[data-start]");console.log(o)}();
+//# sourceMappingURL=02-timer.8f0c8199.js.map

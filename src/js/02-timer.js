016a0bc4df627595b5428c9bc61031b914577fd3
FilipchenkoDima <blewdinor@gmail.com>
2022-11-02 14:41:34 +0200

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,25 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
 const startBtn = document.querySelector('[data-start]');
+const dateTimePicker = document.querySelector('input[type="text"]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+// dateTimePicker.addEventListener('click',)
+
+flatpickr(dateTimePicker, options);
+
 
 console.log(startBtn);
+console.log(dateTimePicker);
